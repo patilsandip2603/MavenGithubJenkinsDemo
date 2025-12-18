22c979bac8c36f733986f2f7e65e53df564b6d3d
Sandip Patil <patilsandip2603@gmail.com>
2025-12-18 17:16:53 +0530

    first commit

---
+++ src/test/java/testingPackage/TestingClass.java
@@ -0,0 +1,33 @@
+package testingPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestingClass 
+{
+	 @Test
+	    public void testOne() 
+	    {
+	        System.out.println("Test One: This test will PASS");
+	    }
+
+	    @Test
+	    public void testTwo() 
+	    {
+	        System.out.println("Test Two: This test will FAIL");
+	        Assert.fail("Intentional failure for learning TestNG");
+	    }
+
+	    @Test
+	    public void testThree() 
+	    {
+	        System.out.println("Test Three: This test will PASS");
+	    }
+}
